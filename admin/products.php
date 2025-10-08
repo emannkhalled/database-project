24d6651f06e22aabbf79a8a1a0e080a9aef1caa3
emankhaledd <emankhaled12357@gmail.com>
2025-10-08 13:05:22 +0300

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products | المنتجات </title>
+    <title>Products </title>
     <style>
         h3{
             font-family: 'Cairo', sans-serif;
